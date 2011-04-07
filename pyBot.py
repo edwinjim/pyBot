705ed8df9fa614df2f87c29b1eae21ea41bb4f93
eeemsi <eeemsi@googlemail.com>
2011-04-08 00:22:35 +0200

    replaced some if and bot reacts on version now

--- pyBot.py
+++ pyBot.py
@@ -35,20 +35,19 @@ while 1:
   data = s.recv (4096)
   if data.find ('PING') != -1:
     s.send ('PONG \r\n')
-
-  if data.find ('PRIVMSG %s :!quit' % CHANNEL) != -1:
+  elif data.find ('PRIVMSG %s :!quit' % CHANNEL) != -1:
     s.send ('QUIT : \r\n')
     s.close()
-
-  if data.find ('PRIVMSG %s :!ping' % CHANNEL) != -1:
+  elif data.find ('PRIVMSG %s :!version' % CHANNEL) != -1:
+    s.send('PRIVMSG %s :i dont have one\r\n' % CHANNEL)
+  elif data.find ('PRIVMSG %s :!ping' % CHANNEL) != -1:
     s.send('PRIVMSG %s :pong\r\n' % CHANNEL)
-
-  if data.find('PRIVMSG %s :' % CHANNEL) != -1:
+  elif data.find('PRIVMSG %s :' % CHANNEL) != -1:
     for i in blacklist:
-      if i in data.split('#bot')[1].replace('\r\n', '').replace(':', '').split(): 
+      if i in data.split(CHANNEL)[1].replace('\r\n', '').replace(':', '').split(): 
         s.send('PRIVMSG %s :bad word detected\r\n' % CHANNEL)
 
-# did some not working blackmagic here
+# on some evening on some day someone did some not working blackmagic here
 #((i + ' ' in data.split('#game-dev')[1].replace('\r\n', '') or len(data.split('#game-dev')[1].split(i)[1].replace('\r\n', ''))==0) or (' ' + i in data.split('#game-dev')[1]  or len(data.split('#game-dev')[1].split(i)[0].replace('\r\n', '') or ':' + i in data.split('#game-dev')[1])==0)):
 #######################
       
