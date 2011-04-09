1ae768d41ce504d9643b4aa37681b7194832fb43
eeemsi <eeemsi@googlemail.com>
2011-04-09 23:03:27 +0200

    did some speed up and bugfixing learning bad words, switched to .format instead of %

--- pyBot.py
+++ pyBot.py
@@ -7,7 +7,7 @@ HOST='127.0.0.1'
 CHANNEL='#bot'
 PORT=6667
 
-NICK='pyBot'
+NICK='pyBot_alpha'
 IDENT=('%s' % NICK)
 REALNAME=('%s' % NICK)
 MODE=('%s +B' % NICK)
@@ -20,57 +20,48 @@ s.send('JOIN %s\r\n' % CHANNEL)
 s.send('MODE %s\r\n' % MODE)
 
 def check_badword():
-  try:
-    fileHandle = open ('badwords.txt', 'r')
-    badwords = fileHandle.read()
-    global blacklist 
-    blacklist = badwords.replace('\r\n','').split(',')
-      
-  except:
-    fileHandle = open ('badwords.txt', 'w')
-  fileHandle.close() 
+    connection = sqlite.connect('badwords.db') 
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM badwords')
 
 def db_write(data):
-  try:
-    fileHandle = open ('badwords.db', 'r')
+    badword = ((data.split('!learn')[1]).replace('\n','').replace('\r','').split()[0])
     connection = sqlite.connect('badwords.db')
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO badwords VALUES (null," %s")' % data )
+    cursor.execute('INSERT INTO badwords VALUES (null," {0}")'.format(badword))
     connection.commit()
+    return badword
    
+def check_dbexist():
+  try:
+    fileHandle = open('badwords.db', 'r')
+    fileHandle.close()
+
   except:
-    connection = sqlite.connect('badwords.db') 
+    connection = sqlite.connect('badwords.db')
     cursor = connection.cursor()
     cursor.execute('CREATE TABLE badwords (id INTEGER PRIMARY KEY,word VARCHAR(50))')
     connection.commit()
-  fileHandle.close()
 
-def learn_badword(data):
-  fileHandle = open ('badwords.txt', 'a')
-  badword = ((data.split('!learn')[1]).replace('\n','').replace('\r','').split()[0]+',')
-  fileHandle.write(badword)
-  fileHandle.close()
-  return badword
+check_dbexist()
 
-check_badword() 
- 
 while 1:
   data = s.recv (4096)
   if data.find ('PING') != -1:
     s.send ('PONG '+ data.split() [1] + '\r\n')
-  elif data.find ('PRIVMSG %s :!quit' % CHANNEL) != -1:
+  elif data.find ('PRIVMSG {0} :!quit'.format(CHANNEL)) != -1:
     s.send ('QUIT : \r\n')
     s.close()
-  elif data.find ('PRIVMSG %s :!version' % CHANNEL) != -1:
-    s.send('PRIVMSG %s :i dont have one\r\n' % CHANNEL)
-  elif data.find ('PRIVMSG %s :!ping' % CHANNEL or 'PRIVMSG %s :pyBot: !ping' % CHANNEL) != -1:
-    s.send('PRIVMSG %s :pong\r\n' % CHANNEL)
-  elif data.find('PRIVMSG %s :' % CHANNEL) != -1:
-    for i in blacklist:
-      if i in data.split(CHANNEL)[1].replace('\r\n', '').replace(':', '').split(): 
-        s.send('PRIVMSG %s :bad word detected\r\n' % CHANNEL)
-  if data.find('PRIVMSG %s :!learn' % CHANNEL) != -1:
-    s.send('PRIVMSG %s :new bad word added: ' % CHANNEL + learn_badword(data) + db_write(data)+ i + '\r\n')
+  elif data.find ('PRIVMSG {0} :!version'.format(CHANNEL)) != -1:
+    s.send('PRIVMSG {0} :i am bleeding edge\r\n'.format(CHANNEL))
+  elif data.find ('PRIVMSG {0} :!ping'.format(CHANNEL)) != -1:
+    s.send('PRIVMSG {0} :pong\r\n'.format(CHANNEL))
+#  elif data.find('PRIVMSG %s :' % CHANNEL) != -1:
+#    for i in blacklist:
+#      if i in data.split(CHANNEL)[1].replace('\r\n', '').replace(':', '').split(): 
+#        s.send('PRIVMSG %s :bad word detected\r\n' % CHANNEL)
+  if data.find('PRIVMSG {0} :!learn'.format(CHANNEL)) != -1:
+    s.send('PRIVMSG {0} :new bad word added: '.format(CHANNEL) + db_write(data) + '\r\n')
 # on some evening on some day someone did some not working blackmagic here
 #((i + ' ' in data.split('#game-dev')[1].replace('\r\n', '') or len(data.split('#game-dev')[1].split(i)[1].replace('\r\n', ''))==0) or (' ' + i in data.split('#game-dev')[1]  or len(data.split('#game-dev')[1].split(i)[0].replace('\r\n', '') or ':' + i in data.split('#game-dev')[1])==0)):
 #######################
